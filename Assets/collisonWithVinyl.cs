7f3bee31c412d1a273d1f649368b5de5746eabeb
SantiagoOrdonez <santiagoordonez23@gmail.com>
2016-09-11 04:53:11 -0400

    initial commit

---
+++ Assets/collisonWithVinyl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class collisonWithVinyl : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.name == "Mesh37_Turntable1_Group1_Model") {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.Play();
+            audio.Play(44100);
+        }
+
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
